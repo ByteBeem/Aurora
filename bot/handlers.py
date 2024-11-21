dc3aa3224930419636b87424787fd4ee0678145f
Bytebeem <donald.mxolisi@proton.me>
2024-11-21 15:03:29 +0200

    Initial commit

--- bot/handlers.py
+++ bot/handlers.py
@@ -5,9 +5,6 @@ from telegram.ext import CallbackContext
 import openai
 import os
 
-from bot.config import Aurora_OpenAI_key
-
-openai.api_key = Aurora_OpenAI_key
 
 async def start(update: Update, context: CallbackContext) -> None:
     user = update.effective_user.first_name
@@ -30,17 +27,3 @@ async def unknown(update: Update, context: CallbackContext) -> None:
         "Oops, I didn't understand that command. 😅 Try /help to see what I can do!"
     )
 
-async def generate_gpt_response(prompt: str) -> str:
-    try:
-        response = await openai.chat.completions.create(
-            model="gpt-4o-mini",  
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant, a friendly friend named Aurora with the nickname Rori, built by Donald Mxolisi."},
-                {"role": "user", "content": prompt},
-            ],
-        )
-        return response['choices'][0]['message']['content'].strip()
-    
-    except Exception as e:
-        print(f"Error generating GPT response: {e}")
-        return "Sorry, I couldn't generate a response right now."
